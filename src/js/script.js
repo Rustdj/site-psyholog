f49645bbbfa60ba9f98cb1bec39d5059fca881f6
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-10-06 23:44:30 +0300

    html layout done

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+
+
+const btn = document.querySelector('#send');
+
+function showModal () {
+    if (btn === showModal) {
+        console.log(showModal);
+    }
+
+    btn.addEventListener('click', () => {
+        btn === showModal;
+    });
+    
+}
+
+showModal();

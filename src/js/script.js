06fc40f1a0196f5cbbd8fe0395685bf00b8afe13
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-10-07 23:14:46 +0300

    add post

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 
+// Отправка mail
 
+
+
+$('form').submit(function() {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: 'smart.php',
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find('input').val('');
+
+
+
+        $('form').trigger('reset');
+    });
+    return false;
+});

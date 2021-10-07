9deb4fa833761c776e97a5813cab98e241c4a3b1
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-10-07 14:23:06 +0300

    add modal and all correct

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,2 @@
 
 
-const btn = document.querySelector('#send');
-
-function showModal () {
-    if (btn === showModal) {
-        console.log(showModal);
-    }
-
-    btn.addEventListener('click', () => {
-        btn === showModal;
-    });
-    
-}
-
-showModal();
